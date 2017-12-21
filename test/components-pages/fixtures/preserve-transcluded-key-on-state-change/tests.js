b566e8fddc91b4dbc646ce9afeb7b662dc140009
Dylan Piercey <pierceydylan@gmail.com>
2017-12-21 12:32:39 -0800

    Add test for transcluded keys.

---
+++ test/components-pages/fixtures/preserve-transcluded-key-on-state-change/tests.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+// issue: #947
+
+describe(path.basename(__dirname), function() {
+    it('should update correctly', function() {
+        var component = window.component;
+        var originalChild = component.getComponent('child');
+
+        // Ensure we can find the child reference and that it has the exposed method.
+        expect(originalChild).to.be.have.property('method');
+
+        // Trigger a force re-render and ensure reference still exists.
+        component.forceUpdate();
+        component.update();
+        expect(component.getComponent('child')).to.eql(originalChild);
+    });
+});
